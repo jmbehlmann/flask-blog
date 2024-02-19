9f89de7d3562dfa40648437f81f6c9f4ebe7103e
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-19 09:50:20 -0600

    home and about route

--- blog.py
+++ blog.py
@@ -4,5 +4,15 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return "<h1>home!</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>abouttttttt!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
