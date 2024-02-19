cba274df9fa79f2fa9e01dd286cf43e9c6e67ab8
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-19 10:04:07 -0600

    hardcoded post data and post loop in template

--- blog.py
+++ blog.py
@@ -2,11 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'James Behlmann',
+        'title': 'Post 1',
+        'content': 'this is the first post',
+        'date_posted': 'February 19, 2024'
+    },
+    {
+        'author': 'James Behlmann',
+        'title': 'Post 2',
+        'content': 'this is the second post',
+        'date_posted': 'February 19, 2024'
+    }
+
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
