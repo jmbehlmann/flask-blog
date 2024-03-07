8e838cac9d7e4c5cda19bf4bc79b1a0909f791c0
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-07 14:42:35 -0600

    install sqlalchemy, set db location

--- blog.py
+++ blog.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 posts = [
     {
